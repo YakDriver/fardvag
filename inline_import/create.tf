eae40f1cd1e6d96e56ebd8eceb829c343891ba13
Dirk Avery <dirk.avery@gmail.com>
2018-08-28 14:10:03 -0400

    Add testing on importing inline routes

---
+++ inline_import/create.tf
@@ -0,0 +1,36 @@
+variable "vpc_id" {}
+variable "vpc_peering_conn_1" {}
+variable "vpc_peering_conn_2" {}
+variable "rt_name" {}
+
+variable "destination_cidr_ipv4" {}
+variable "destination_cidr_ipv6" {}
+
+resource "aws_route_table" "rt" {
+  vpc_id = "${var.vpc_id}"
+
+  route {
+    cidr_block = "${var.destination_cidr_ipv4}"
+    vpc_peering_connection_id = "${var.vpc_peering_conn_1}"
+  }
+
+  route {
+    ipv6_cidr_block = "${var.destination_cidr_ipv6}"
+    vpc_peering_connection_id = "${var.vpc_peering_conn_2}"
+  }  
+
+  tags {
+    Name = "${var.rt_name}"
+  }
+}
+
+resource "aws_route" "route_1" {
+  count = 0
+  route_table_id = "${aws_route_table.rt.id}"
+  destination_cidr_block = "${var.destination_cidr_ipv4}"
+  vpc_peering_connection_id = "${var.vpc_peering_conn_1}"
+}
+
+output "rt_id" {
+  value = "${aws_route_table.rt.id}"
+}
